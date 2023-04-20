08846e6943079b99249d795b7d702b9804b050eb
baobao1022002 <117251017+baobao1022002@users.noreply.github.com>
2023-04-20 18:45:19 +0700

    'chucNangMenu'

---
+++ app/src/main/java/vn/name/appbanhang/utils/Utils.java
@@ -0,0 +1,11 @@
+package vn.name.appbanhang.utils;
+
+public class Utils {
+    public static final String BASE_URL="http://192.168.207.152/banhang/";
+//192.168.103.152
+// 192.168.111.152
+//    192.168.139.1
+// 192.168.190.1
+//    192.168.50.126
+//192.168.89.126
+}
