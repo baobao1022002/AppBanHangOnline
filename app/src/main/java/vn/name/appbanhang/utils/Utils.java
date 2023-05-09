ff856423b589142cf9e03ec3298ea2d419a47cb6
Nguyễn Võ Trường Thạnh <93345772+Nguyen-Vo-Truong-Thanh@users.noreply.github.com>
2023-05-09 22:15:00 +0700

    Add files via upload

--- app/src/main/java/vn/name/appbanhang/utils/Utils.java
+++ app/src/main/java/vn/name/appbanhang/utils/Utils.java
@@ -1,5 +1,6 @@
 package vn.name.appbanhang.utils;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import vn.name.appbanhang.model.GioHang;
@@ -8,5 +9,6 @@ import vn.name.appbanhang.model.User;
 public class Utils {
     public static final String BASE_URL="http://192.168.1.2/banhang/";
     public static List<GioHang> manggiohang;
+    public static List<GioHang> mangmuahang = new ArrayList<>();
     public static User user_current = new User();
 }
