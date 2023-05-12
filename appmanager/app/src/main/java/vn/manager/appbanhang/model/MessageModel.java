7e70984f8985e2e88418a497498c1eba5b2d8f6a
baobao1022002 <117251017+baobao1022002@users.noreply.github.com>
2023-05-12 11:34:18 +0700

    'themsp'

---
+++ appmanager/app/src/main/java/vn/manager/appbanhang/model/MessageModel.java
@@ -0,0 +1,22 @@
+package vn.manager.appbanhang.model;
+
+public class MessageModel {
+    boolean success;
+    String message;
+
+    public boolean isSuccess() {
+        return success;
+    }
+
+    public void setSuccess(boolean success) {
+        this.success = success;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+}
--- appmanager/app/src/main/java/vn/manager/appbanhang/utils/Utils.java
+++ appmanager/app/src/main/java/vn/manager/appbanhang/utils/Utils.java
@@ -7,7 +7,7 @@ import vn.manager.appbanhang.model.GioHang;
 import vn.manager.appbanhang.model.User;
 
 public class Utils {
-    public static final String BASE_URL="http://192.168.1.6/banhang/";
+    public static final String BASE_URL="http://192.168.135.152/banhang/";
     public static List<GioHang> manggiohang;
     public static List<GioHang> mangmuahang = new ArrayList<>();
     public static User user_current = new User();
