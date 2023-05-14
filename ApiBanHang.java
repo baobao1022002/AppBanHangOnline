a39fa81e4c71c19fac77b5a695a654412259ba20
Nguyễn Võ Trường Thạnh <93345772+Nguyen-Vo-Truong-Thanh@users.noreply.github.com>
2023-05-14 10:49:48 +0700

    Add files via upload

---
+++ ApiBanHang.java
@@ -0,0 +1,93 @@
+package vn.manager.appbanhang.retrofit;
+
+import io.reactivex.rxjava3.core.Observable;
+
+import okhttp3.MultipartBody;
+import retrofit2.Call;
+import retrofit2.http.Field;
+import retrofit2.http.FormUrlEncoded;
+import retrofit2.http.GET;
+import retrofit2.http.Multipart;
+import retrofit2.http.POST;
+import retrofit2.http.Part;
+import vn.manager.appbanhang.model.DonHangModel;
+import vn.manager.appbanhang.model.LoaiSpModel;
+import vn.manager.appbanhang.model.MessageModel;
+import vn.manager.appbanhang.model.SanPhamMoiModel;
+import vn.manager.appbanhang.model.UserModel;
+
+public interface ApiBanHang {
+    @GET("getloaisp.php")
+    Observable<LoaiSpModel> getLoaiSp();
+
+    @GET("getspmoi.php")
+    Observable<SanPhamMoiModel> getSpMoi();
+
+   @POST("chitiet.php")
+   @FormUrlEncoded
+   Observable<SanPhamMoiModel> getSanPham(
+          @Field("page") int page,
+          @Field("loai") int loai
+   );
+
+    @POST("dangki.php")
+    @FormUrlEncoded
+    Observable<UserModel> dangKi(
+            @Field("email") String email,
+            @Field("pass") String pass,
+            @Field("username") String username,
+            @Field("mobile") String mobile
+    );
+
+    @POST("dangnhap.php")
+    @FormUrlEncoded
+    Observable<UserModel> dangNhap(
+            @Field("email") String email,
+            @Field("pass") String pass
+    );
+
+    @POST("reset.php")
+    @FormUrlEncoded
+    Observable<UserModel> resetPass(
+            @Field("email") String email
+    );
+
+    @POST("donhang.php")
+    @FormUrlEncoded
+    Observable<UserModel> createOrder(
+            @Field("email") String email,
+            @Field("sdt") String sdt,
+            @Field("tongtien") String tongtien,
+            @Field("iduser") int id,
+            @Field("diachi") String diachi,
+            @Field("soluong") int soluong,
+            @Field("chitiet") String chitiet
+    );
+
+    @POST("xemdonhang.php")
+    @FormUrlEncoded
+    Observable<DonHangModel> xemDonHang(
+            @Field("iduser") int id
+    );
+
+    @POST("timkiem.php")
+    @FormUrlEncoded
+    Observable<SanPhamMoiModel> search(
+            @Field("search") String search
+    );
+
+    @POST("insertsp.php")
+    @FormUrlEncoded
+    Observable<MessageModel> insertSp(
+            @Field("tensp") String tensp,
+            @Field("gia") String gia,
+            @Field("hinhanh") String hinhanh,
+            @Field("mota") String mota,
+            @Field("loai") int id
+    );
+
+    @Multipart
+    @POST("upload.php")
+    Call<MessageModel> uploadFile(@Part MultipartBody.Part file);
+
+}
